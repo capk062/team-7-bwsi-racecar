1eb15c99f6d540c68a53fc043ebfc5f9a4572d8e
jc2216 <jc2216789@gmail.com>
2026-07-18 23:46:39 -0400

    it's updated

--- line_follower_real.py
+++ line_follower_real.py
@@ -54,14 +54,15 @@ import racecar_utils as rc_utils
 
 rc = racecar_core.create_racecar()
 
-kP = -0.001125   # maybe need to increase current kp hopefully will work
-MAX_SPEED = 1  
-kD = 0#-0.000001425
-MIN_CONTOUR_AREA = 30
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+kP = config["kP"]
+kD = config["kD"]
+MIN_CONTOUR_AREA = config["MIN_CONTOUR_AREA"]
+MAX_SPEED = config["MAX_SPEED"]
                     
 # >> Constants
-# The smallest contour we will recognize as a valid contour
-
 
 # A crop window for the floor directly in front of the car
 CROP_FLOOR = ((300, 0), (rc.camera.get_height(), rc.camera.get_width()))
@@ -90,9 +91,7 @@ def update_contour():
    global contour_center
    global contour_area
 
-
    image = rc.camera.get_color_image()
-
 
    if image is None:
        contour_center = None
@@ -100,12 +99,8 @@ def update_contour():
    else:
        # Crop the image to the floor directly in front of the car
        image = rc_utils.crop(image, CROP_FLOOR[0], CROP_FLOOR[1])
-
-
        # TODO Part 2: Search for line colors, and update the global variables
        # contour_center and contour_area with the largest contour found. shorten w/ function
-
-
        contours = rc_utils.find_contours(image, BLUE[0], BLUE[1])
        contour = rc_utils.get_largest_contour(contours, MIN_CONTOUR_AREA)
 
@@ -123,43 +118,22 @@ def update_contour():
        #rc.display.show_color_image(image)
 
 
-
-
-
-
 # [FUNCTION] The start function is run once every time the start button is pressed
 def start():
    global speed
    global angle
    global last_error
 
-
    # Initialize variables
    speed = 0
    angle = 0
    last_error = 0
 
-
    # Set initial driving speed and angle
    rc.drive.set_speed_angle(speed, angle)
 
-   #rc.telemetry.declare_variables("angle", "contour_center")
-
-
    # Set update_slow to refresh every half second
    rc.set_update_slow_time(0.5)
-
-
-   # Print start message
-   print(
-       ">> Lab 2A - Color Image Line Following\n"
-       "\n"
-       "Controls:\n"
-       "   Right trigger = accelerate forward\n"
-       "   Left trigger = accelerate backward\n"
-       "   A button = print current speed and angle\n"
-       "   B button = print contour center and area"
-   )
 
 
 # [FUNCTION] After start() is run, this function is run once every frame (ideally at
@@ -176,32 +150,20 @@ def update():
    global last_error
    
    rc.drive.set_max_speed(MAX_SPEED)
-   #speed = 1
+   speed = 1
 
    # Search for contours in the current color image
    update_contour()
-
    
    if contour_center is not None:
        setpoint = 360
        present_value = contour_center[1]
        error = setpoint - present_value
        angle = kP * error + kD * (error - last_error)/rc.get_delta_time()
-
        angle = rc_utils.clamp(angle, -1, 1)
-  
-   # Use the triggers to control the car's speed
-   #rt = rc.controller.get_trigger(rc.controller.Trigger.RIGHT)
-   #lt = rc.controller.get_trigger(rc.controller.Trigger.LEFT)
+
    last_error = error
-   speed = 1
-
-   #rc.telemetry.record(angle, contour_center)
-   #rc.telemetry.visualize()
    rc.drive.set_speed_angle(speed, angle)
-
-
-   # Print the current speed and angle when the A button is held down
 
 
 # [FUNCTION] update_slow() is similar to update() but is called once per second by
