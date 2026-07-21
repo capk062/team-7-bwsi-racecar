8e7963ca2c3fbc1f7979b3f1ff4bc822d759d0a4
jc2216 <jc2216789@gmail.com>
2026-07-20 23:52:56 -0400

    added some files with no code

---
+++ imu_investigation/pose_estimation_node.py
@@ -0,0 +1,5 @@
+"""
+this is the pose estimation node.
+
+we'll see how it works out
+"""
---
+++ imu_investigation/velocity_calculation_node.py
@@ -0,0 +1,9 @@
+"""
+this is a velocity calculation node
+
+it will integrate acceleration over time.
+
+first, subtract gravity readings.
+
+also kalman filter somewhere
+"""
